bb2793bf8d1900b1b9942a7c55a6e1c040523246
Maksymilian Kula <maksymiliankula@wp.plgit config --global user.email>
2026-04-23 00:34:54 +0200

    feat: Timer std::chrono microseconds

---
+++ src/utils/Timer.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <chrono>
+
+// Czas w mikrosekundach
+//   Timer t
+//   t.start()
+//   sort(data, size)
+//   t.stop()
+//   long long us = t.elapsed()
+
+class Timer
+{
+public:
+    void start()
+    {
+        m_begin = std::chrono::high_resolution_clock::now();
+    }
+
+    void stop()
+    {
+        m_end = std::chrono::high_resolution_clock::now();
+    }
+
+    //zwraca czas w mikrosekundach
+    long long elapsed() const
+    {
+        return std::chrono::duration_cast<std::chrono::microseconds>(m_end - m_begin).count();
+    }
+
+private:
+    std::chrono::high_resolution_clock::time_point m_begin;
+    std::chrono::high_resolution_clock::time_point m_end;
+};
