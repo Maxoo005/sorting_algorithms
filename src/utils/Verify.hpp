31626acc831e6cba13054bd1176516ca974a3971
Maksymilian Kula <maksymiliankula@wp.plgit config --global user.email>
2026-04-26 13:29:33 +0200

    feat: add Verify (isSorted, check)

---
+++ src/utils/Verify.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+
+namespace Verify
+{
+    // true jesli sortowane poprawnie
+    template <typename T>
+    bool isSorted(const T *data, int size)
+    {
+        for (int i = 0; i < size - 1; ++i)
+        {
+            if (data[i] > data[i + 1])
+                return false;
+        }
+        return true;
+    }
+
+    // wypisuje czy sortowanie poprawne
+    template <typename T>
+    void check(const T *data, int size)
+    {
+        if (isSorted(data, size))
+            std::cout << "OK: posortowane poprawnie.\n";
+        else
+            std::cout << "BŁĄD: sortowanie niepoprawne!\n";
+    }
+}
